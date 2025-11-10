3f07794c9ff44f3f708764dcbf23a8c99c31ec29
Olah1996 <kadloyentreprise2801@gmail.com>
2025-11-10 14:47:17 -0700

    Third comment

--- script.js
+++ script.js
@@ -4,11 +4,23 @@ document.addEventListener('DOMContentLoaded' , function(){
 
     // Get DOM ELEMENT 
 const hamburger = document.querySelector('.hamburger');
-const navMenu = document.querySelector('.nav-Menu');
+const navMenu = document.querySelector('.menu');
 
 hamburger.addEventListener('click' , function(){
     navMenu.classList.toggle('active');
     hamburger.classList.toggle('active');
+});
+
+// Handle window resize: auto-close mobile menu on desktop view
+
+window.addEventListener('resize' , function(){
+    if (this.window.innerWidth > 768){
+        // on desktop , ensure menu is visible and hambugger is reset
+
+        navMenu.classList.remove('active')
+        hamburger.classList.remove('active')
+
+    };
 });
 
 
